6dba4ca943893125f28427a6123786ac52d9604a
Giovanni Ciotta <giociotta@gmail.com>
2022-09-12 10:03:57 +0200

    visitseed

--- database/seeds/VisitSeeder.php
+++ database/seeds/VisitSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Visit;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class VisitSeeder extends Seeder
 {
@@ -9,8 +11,16 @@ class VisitSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i<50; $i++) {
+            $visit = new Visit();
+
+            // $visit -> apartments_id = $faker -> ;
+
+            $visit -> date = $faker -> date('Y_m_d');
+            $visit -> ip_adress = $faker -> localIpv4();
+            $visit -> save();
+        }
     }
 }
