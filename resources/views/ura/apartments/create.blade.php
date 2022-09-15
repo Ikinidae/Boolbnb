32be0b981609bad6b3b4fc502fdcbabd3d91b17c
Giovanni Ciotta <giociotta@gmail.com>
2022-09-15 10:46:06 +0200

    * create

--- resources/views/ura/apartments/create.blade.php
+++ resources/views/ura/apartments/create.blade.php
@@ -8,7 +8,7 @@
         @csrf
 
         <div class="mb-3">
-            <label class="form-label" for="title">Title</label>
+            <label class="form-label" for="title">Title *</label>
             <input class="form-control @error('title') is-invalid @enderror" type="text" name="title" id="title" value="{{ old('title') }}">
             @error('title')
                 <div class="invalid-feedback">
@@ -18,7 +18,7 @@
         </div>
 
         <div class="mb-3">
-            <label class="form-label" for="description">Description</label>
+            <label class="form-label" for="description">Description *</label>
             <input class="form-control @error('description') is-invalid @enderror" type="text" name="description" id="description" value="{{ old('description') }}">
             {{-- <button type="button" class="btn btn-primary">Reset</button> --}}
             @error('description')
@@ -29,7 +29,7 @@
         </div>
 
         <div class="mb-3">
-            <label class="form-label" for="price">Price</label>
+            <label class="form-label" for="price">Price*</label>
             <input class="form-control @error('price') is-invalid @enderror" type="text" name="price" id="price" value="{{ old('price') }}">
             {{-- <button type="button" class="btn btn-primary">Reset</button> --}}
             @error('price')
@@ -40,7 +40,7 @@
         </div>
 
         <div class="mb-3">
-            <label class="form-label" for="rooms">Rooms</label>
+            <label class="form-label" for="rooms">Rooms*</label>
             <input class="form-control @error('rooms') is-invalid @enderror" type="text" name="rooms" id="rooms" value="{{ old('rooms') }}">
             {{-- <button type="button" class="btn btn-primary">Reset</button> --}}
             @error('rooms')
@@ -51,7 +51,7 @@
         </div>
 
         <div class="mb-3">
-            <label class="form-label" for="beds">Beds</label>
+            <label class="form-label" for="beds">Beds*</label>
             <input class="form-control @error('beds') is-invalid @enderror" type="text" name="beds" id="beds" value="{{ old('beds') }}">
             {{-- <button type="button" class="btn btn-primary">Reset</button> --}}
             @error('beds')
@@ -62,7 +62,7 @@
         </div>
 
         <div class="mb-3">
-            <label class="form-label" for="bathrooms">Bathrooms</label>
+            <label class="form-label" for="bathrooms">Bathrooms*</label>
             <input class="form-control @error('bathrooms') is-invalid @enderror" type="text" name="bathrooms" id="bathrooms" value="{{ old('bathrooms') }}">
             {{-- <button type="button" class="btn btn-primary">Reset</button> --}}
             @error('bathrooms')
@@ -73,7 +73,7 @@
         </div>
 
         <div class="mb-3">
-            <label class="form-label" for="mq">mq</label>
+            <label class="form-label" for="mq">mq*</label>
             <input class="form-control @error('mq') is-invalid @enderror" type="text" name="mq" id="mq" value="{{ old('mq') }}">
             {{-- <button type="button" class="btn btn-primary">Reset</button> --}}
             @error('mq')
@@ -84,7 +84,7 @@
         </div>
 
         <div class="mb-3">
-            <label class="form-label" for="address">Address</label>
+            <label class="form-label" for="address">Address*</label>
             <input onkeyup="search()" class="form-control @error('address') is-invalid @enderror" type="text" name="address" id="query" value="{{ old('address') }}">
                 {{-- <input onkeyup="search()"  type="text" id="query" value=""> --}}
                 {{-- <button onclick="search()">SEARCH</button> --}}
@@ -117,7 +117,7 @@
         </div> --}}
 
         <div class="mb-3">
-            <label class="form-label" for="image">Image</label>
+            <label class="form-label" for="image">Image*</label>
             <input class="form-control @error('image') is-invalid @enderror" type="file" name="image" id="image" accept="image/*">
             @error('image')
                 <div class="invalid-feedback">
@@ -130,7 +130,7 @@
 
         <div class="mb-3">
             <div>
-                <label for="visible">Visible</label>
+                <label for="visible">Visible*</label>
             </div>
             <div class="form-check">
                 <input class="form-check-input @error('flexRadioDefault1') is-invalid @enderror" type="radio" name="visible" id="flexRadioDefault1" value="1">
