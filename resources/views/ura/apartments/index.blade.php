cf60c8b406bf527158a4fd0a6b2094e75afba91f
Giovanni Ciotta <giociotta@gmail.com>
2022-09-20 11:27:07 +0200

    alert creted-deleted

--- resources/views/ura/apartments/index.blade.php
+++ resources/views/ura/apartments/index.blade.php
@@ -3,13 +3,13 @@
 
 @section('mainContent')
     @if (session('deleted'))
-        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <div class="alert  alert-danger alert-dismissible fade show" role="alert">
             {!! session('deleted') !!}
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     @endif
     @if (session('created'))
-        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
             {!! session('created') !!}
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
