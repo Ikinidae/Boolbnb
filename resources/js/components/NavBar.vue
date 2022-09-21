a8b093a13fc76ca6d03eeacefda4f882768fa4b6
Giovanni Ciotta <giociotta@gmail.com>
2022-09-21 12:56:29 +0200

    navbar mod

--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -6,7 +6,7 @@
                 <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse d-flex flex-column" id="navbarTogglerDemo03">
+                <div class="collapse navbar-collapse d-flex" id="navbarTogglerDemo03">
                 <!-- <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
@@ -33,11 +33,50 @@
                             id="query"
                             v-model="searchAddress"
                         />
+
                     </div>
+                    <div class="dropdown m-3 ">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                            filter*
+                            </button>
+  <ul class="dropdown-menu " aria-labelledby="dropdownMenuButton1">
+    <li>  <label  for="radius">Choose radius distance</label>
+                        <select class="me-2" @change="getApartments(radius,latitude,longitude)" name="radius" id="radius" v-model="radius">
+                            <option value="10">10</option>
+                            <option value="20">20</option>
+                            <option value="30">30</option>
+                            <option value="40">40</option>
+                            <option value="50">50</option>
+                        </select>
+    </li>
+    <li>
+        <label for="rooms">Choose min rooms</label>
+                        <select class="me-2" @change="selectRooms" name="rooms" id="rooms" v-model="rooms">
+                            <option value="Any">Any</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+    </li>
+    <li>
+        <label for="beds">Choose min beds</label>
+                        <select class="me-2" @change="" name="beds" id="beds" v-model="beds">
+                            <option value="Any">Any</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+    </li>
+  </ul>
+</div>
 
 
                     <!-- Input per radiante -->
-                    <div class="m-2">
+                    <!-- <div class="m-2">
                         <label  for="radius">Choose radius distance</label>
                         <select class="me-3" @change="getApartments(radius,latitude,longitude)" name="radius" id="radius" v-model="radius">
                             <option value="10">10</option>
@@ -66,7 +105,7 @@
                             <option value="4">4</option>
                             <option value="5">5</option>
                         </select>
-                    </div>
+                    </div> -->
 
 
                     <!-- nel button viene fatta partire la funzione getApartments(lat long radius) con i valori di lat lon e radius  -->
