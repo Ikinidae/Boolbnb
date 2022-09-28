078d8a48a5a78e26c314340367f85b7774055b59
Tommaso <tommaso.lodesani@gmail.com>
2022-09-28 10:28:23 +0200

    fix table index

--- resources/views/ura/apartments/index.blade.php
+++ resources/views/ura/apartments/index.blade.php
@@ -16,56 +16,47 @@
     @endif
 
     <h1>Apartments</h1>
-    @forelse ($apartments as $apartment)
+
+
     <div class="table-responsive">
-
-
-            <table class="table table-md table-striped table-hover ">
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Title</th>
-                        <th>Description</th>
-                        <th>Price</th>
-                        <th>Rooms</th>
-                        <th>Beds</th>
-                        <th>Bathrooms</th>
-                        <th>mq</th>
-                        <th>Address</th>
-                        <th>Services</th>
-                        <th>Visible</th>
+        <table class="table table-md table-striped table-hover ">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Price</th>
+                    <th>Rooms</th>
+                    <th>Beds</th>
+                    <th>Bathrooms</th>
+                    <th>mq</th>
+                    <th>Address</th>
+                    <th>Services</th>
+                    <th>Visible</th>
 
                     <th colspan="3">Actions</th>
                 </tr>
             </thead>
             <tbody>
-                <tr data-id="{{ $apartment->id }}">
-                    <td>{{ $apartment->id }}</td>
-                    <td>{{ $apartment->title }}</td>
-                    <td>{{ $apartment->description }}</td>
-                    <td>{{ $apartment->price }}</td>
-                    <td>{{ $apartment->rooms }}</td>
-                    <td>{{ $apartment->beds }}</td>
-                    <td>{{ $apartment->bathrooms }}</td>
-                    <td>{{ $apartment->mq }}</td>
-                    <td>{{ $apartment->address }}</td>
+                @forelse ($apartments as $apartment)
+                    <tr data-id="{{ $apartment->id }}">
+                        <td>{{ $apartment->id }}</td>
+                        <td>{{ $apartment->title }}</td>
+                        <td>{{ $apartment->description }}</td>
+                        <td>{{ $apartment->price }}</td>
+                        <td>{{ $apartment->rooms }}</td>
+                        <td>{{ $apartment->beds }}</td>
+                        <td>{{ $apartment->bathrooms }}</td>
+                        <td>{{ $apartment->mq }}</td>
+                        <td>{{ $apartment->address }}</td>
 
-                    <td>
-                        @foreach ($apartment->services as $service)
-                            <span class="tag">{{ $service->name }}</span>
-                        @endforeach
-                    </td>
-                    {{-- <td>{{ $apartment->services }}</td> --}}
-                    <td>{{ $apartment->visible }}</td>
+                        <td>
+                            @foreach ($apartment->services as $service)
+                                <span class="tag">{{ $service->name }}</span>
+                            @endforeach
+                        </td>
 
-
-                        {{-- SOSTITUIRE CATEGORY CON SERVICES --}}
-                        {{-- <td>
-                                <a href="{{ route('admin.categories.show', ['category' => $apartment->category]) }}">
-                                {{ $apartment->category->name }}
-                                </a>
-                            </td> --}}
-
+                        <td>{{ $apartment->visible }}</td>
 
                         <td>
                             <a href="{{ route('ura.apartments.show', ['apartment' => $apartment]) }}"
@@ -76,7 +67,8 @@
                                 class="btn btn-warning">Edit</a>
                         </td>
                         <td>
-                            <form action="{{ route('ura.apartments.destroy', ['apartment' => $apartment]) }}" method="post">
+                            <form action="{{ route('ura.apartments.destroy', ['apartment' => $apartment]) }}"
+                                method="post">
                                 @csrf
                                 @method('DELETE')
                                 <button type="submit" class="btn btn-danger"
@@ -84,30 +76,15 @@
                             </form>
                         </td>
                         <td>
-                            <a href="{{route('ura.messages.show', ['message' => $apartment->id] )}}"
+                            <a href="{{ route('ura.messages.show', ['message' => $apartment->id]) }}"
                                 class="btn btn-warning">Messages</a>
                         </td>
                     </tr>
-                </tbody>
-            </table>
+                @empty
+                    <p>No apartments</p>
+                @endforelse
+            </tbody>
+        </table>
     </div>
 
-    @empty
-        <p>No apartments</p>
-    @endforelse
-
-
-
-    {{-- {{ $posts->links() }} --}}
-
-    {{-- <section class="overlay d-none">
-        <form class="popup" data-action="{{ route('ura.apartaments.destroy', ['apartament' => '*****']) }}" method="post">
-            @csrf
-            @method('DELETE')
-
-            <h1>Sei sicuro?</h1>
-            <button type="submit" class="btn btn-warning js-yes">Yes</button>
-            <button type="button" class="btn btn-danger js-no">No</button>
-        </form>
-    </section> --}}
 @endsection
